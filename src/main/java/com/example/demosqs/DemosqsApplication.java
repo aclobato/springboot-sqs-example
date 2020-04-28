5f08610787818a1fabee28255e2b64cc4e3d9b8e
Ana Carolina Lobato <carolina.lobato@dtidigital.com.br>
2020-04-28 12:46:51 -0300

    Disable certificate checking

--- src/main/java/com/example/demosqs/DemosqsApplication.java
+++ src/main/java/com/example/demosqs/DemosqsApplication.java
@@ -1,5 +1,6 @@
 package com.example.demosqs;
 
+import com.amazonaws.SDKGlobalConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemosqsApplication {
 
 	public static void main(String[] args) {
+		System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true");
 		SpringApplication.run(DemosqsApplication.class, args);
 	}
 
